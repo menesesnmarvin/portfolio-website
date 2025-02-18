64fd5903904efbb215e8b60b45763466a62cc764
Marvin Meneses <Marvin.Meneses@wdc.com>
2025-02-18 08:43:04 +0800

    update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -22,9 +22,50 @@ const Skills = () => {
           </div>
           <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
             <div className="h-14 w-14">
+              <img src="/reactnative-inner.svg" />
+            </div>
+          </div>
+          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
+            <div className="h-14 w-14">
               <svg viewBox="0 0 128 128">
                 <path d="M30.2 45.9h24.1v1.9H32.4v14.4H53v1.9H32.4v15.8h22.2v1.9H30.2V45.9zm26.3 0h2.6l11.4 15.8L82 45.9l15.8-20-26 37.5 13.4 18.4h-2.7L70.4 65 58.2 81.8h-2.6l13.5-18.4-12.6-17.5zm29.7 1.9v-1.9h27.5v1.9H101v34h-2.2v-34H86.2zM0 45.9h2.7l38.2 56.8-15.8-20.9L2.3 48.6l-.1 33.2H0zm113.5 33.4c.5 0 .8-.3.8-.8s-.3-.8-.8-.8-.8.3-.8.8.4.8.8.8zm2.2-2.1c0 1.3 1 2.2 2.4 2.2 1.5 0 2.4-.9 2.4-2.5v-5.5h-1.2v5.5c0 .9-.4 1.3-1.2 1.3-.7 0-1.2-.4-1.2-1.1h-1.2zm6.3-.1c.1 1.4 1.2 2.3 3 2.3s3-.9 3-2.4c0-1.2-.7-1.8-2.2-2.2l-.9-.2c-1-.2-1.4-.6-1.4-1.1 0-.7.6-1.2 1.6-1.2.9 0 1.5.4 1.6 1.2h1.2c-.1-1.3-1.2-2.2-2.8-2.2-1.7 0-2.8.9-2.8 2.3 0 1.1.6 1.8 2 2.1l1 .2c1 .2 1.5.6 1.5 1.2 0 .7-.7 1.2-1.7 1.2s-1.8-.5-1.9-1.2H122z"></path>
               </svg>
+            </div>
+          </div>
+          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
+            <div className="h-14 w-14">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433"
+                className="h-14"
+                alt="JavaScript"
+              />
+            </div>
+          </div>
+          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
+            <div className="h-14 w-14 flex items-center flex-col">
+              <img
+                src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg"
+                alt="TypeScript"
+              />
+            </div>
+          </div>
+
+          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
+            <div className="h-14 w-14">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/130px-HTML5_logo_and_wordmark.svg.png"
+                alt="HTML5"
+                className="h-14"
+              />
+            </div>
+          </div>
+          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
+            <div className="h-14 w-14 flex justify-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/120px-CSS3_logo_and_wordmark.svg.png"
+                className="h-14"
+                alt="CSS"
+              />
             </div>
           </div>
           <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
@@ -48,33 +89,6 @@ const Skills = () => {
             </div>
           </div>
           <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
-            <div className="h-14 w-14">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433"
-                className="h-14"
-                alt="JavaScript"
-              />
-            </div>
-          </div>
-          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
-            <div className="h-14 w-14">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/130px-HTML5_logo_and_wordmark.svg.png"
-                alt="HTML5"
-                className="h-14"
-              />
-            </div>
-          </div>
-          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
-            <div className="h-14 w-14 flex justify-center">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/120px-CSS3_logo_and_wordmark.svg.png"
-                className="h-14"
-                alt="CSS"
-              />
-            </div>
-          </div>
-          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
             <div className="h-14 w-14 flex flex-col justify-center items-center">
               <img
                 src="https://mui.com/static/logo.png"
@@ -83,6 +97,14 @@ const Skills = () => {
               />
               <span className="text-sm dark:text-black text-black">
                 MaterialUI
+              </span>
+            </div>
+          </div>
+          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
+            <div className="h-14 w-14 flex flex-col justify-center items-center">
+              <img src="/primereact.png" className="h-14" alt="PrimeReact" />
+              <span className="text-sm dark:text-black text-black">
+                PrimeReact
               </span>
             </div>
           </div>
@@ -252,6 +274,15 @@ const Skills = () => {
                   d="M64.242 73.542l-.05-.009-.051.008.119-.427-.018.428z"
                 ></path>
               </svg>
+            </div>
+          </div>
+          <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
+            <div className="h-14 w-14 flex items-center flex-col">
+              <label className="text-xs">Redis</label>
+              <img
+                src="https://www.vectorlogo.zone/logos/redis/redis-icon.svg"
+                alt="JavaScript"
+              />
             </div>
           </div>
           <div className="bg-white drop-shadow-md p-5 mb-5 mr-5 rounded-md">
